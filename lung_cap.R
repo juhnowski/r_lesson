5f59b07a9dfcb0d553d8cb950e055d8b2951a330
juhnowski <juhnowski@gmail.com>
2019-02-11 03:40:56 +0300

    before linear

--- lung_cap.R
+++ lung_cap.R
@@ -43,3 +43,49 @@ plot( FEV ~ Age,
       # \n means `new line'
       ylab="FEV (in L)", xlab="Age (in years)",
       ylim=c(0, 6), xlim=c(0, 20), las=1)
+
+plot( FEV ~ Age,
+      data=subset(lungcap, Smoke=="Non-smoker"), # Only select non-smokers
+      main="FEV vs age\nfor non-smokers",
+      ylab="FEV (in L)", xlab="Age (in years)",
+      ylim=c(0, 6), xlim=c(0, 20), las=1)
+
+plot( FEV ~ Ht, data=subset(lungcap, Smoke=="Smoker"),
+      main="FEV vs height\nfor smokers",
+      ylab="FEV (in L)", xlab="Height (in inches)",
+      xlim=c(45, 75), ylim=c(0, 6), las=1)
+
+plot( FEV ~ Ht, data=subset(lungcap, Smoke=="Non-smoker"),
+      main="FEV vs height\nfor non-smokers",
+      ylab="FEV (in L)", xlab="Height (in inches)",
+      xlim=c(45, 75), ylim=c(0, 6), las=1)
+
+AgeAdjust <- lungcap$Age + ifelse(lungcap$Smoke=="Smoker", 0, 0.5)
+
+plot( FEV ~ AgeAdjust, data=lungcap,
+      pch = ifelse(Smoke=="Smoker", 3, 20),
+      xlab="Age (in years)", ylab="FEV (in L)", main="FEV vs age", las=1)
+
+boxplot(lungcap$FEV ~ lungcap$Smoke + lungcap$Gender,
+        ylab="FEV (in L)", main="FEV, by gender\n and smoking status",
+        las=2,
+        # Keeps labels perpendicular to the axes
+        names=c("F:\nNon", "F:\nSmoker", "M:\nNon", "M:\nSmoker"))
+
+interaction.plot( lungcap$Smoke, lungcap$Gender, lungcap$FEV,
+                  xlab="Smoking status", ylab="FEV (in L)",
+                  main="Mean FEV, by gender\n and smoking status",
+                  trace.label="Gender", las=1)
+
+interaction.plot( lungcap$Smoke, lungcap$Gender, lungcap$Age,
+                  xlab="Smoking status", ylab="Age (in years)",
+                  main="Mean age, by gender\n and smoking status",
+                  trace.label="Gender", las=1)
+
+contrasts( relevel( lungcap$Gender, "M") )
+contrasts(lungcap$Gender)
+
+lungcap$Smoke <- factor(lungcap$Smoke,
+                        levels=c(0, 1),
+                        labels=c("Non-smoker","Smoker"))
+contrasts(lungcap$Smoke)
